e6b81d72cefebaaacefe515f3a66f36c0c7e6fa7
201580691 <201580691@605-15-PC>
2016-12-05 14:21:37 +0900

    첫번째과제

---
+++ SpringCRUDExam/src/main/java/egovframework/student/service/impl/StudentServiceImpl.java
@@ -0,0 +1,36 @@
+package egovframework.student.service.impl;
+
+import java.util.List;
+
+
+import javax.inject.Inject;
+
+import org.springframework.stereotype.Service;
+
+
+
+import egovframework.student.service.StudentService;
+import egovframework.student.StudentVO;
+import egovframework.student.service.StudentDAO;
+
+
+@Service
+public class StudentServiceImpl implements StudentService {
+        
+
+
+    @Inject
+    private StudentDAO studentDAO;
+    
+    public void insertStudent(StudentVO vo) throws Exception {
+    	
+    	
+    	studentDAO.insertStudent(vo);
+    	
+    }
+    public List<StudentVO> selectStudentList() throws Exception{
+    	return studentDAO.selectStudent();
+  
+	}
+    
+}
